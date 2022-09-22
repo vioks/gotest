3fc33ef5ba52d83d4f4f036c00accf1c3965fc25
wonwon2 <chzhffpt1996@naver.com>
2022-09-22 11:45:02 +0900

    won  test

--- main.go
+++ main.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"reflect"
-	"strconv"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -46,55 +44,6 @@ func main() {
 	})
 
 	e.POST("/query", handler.Query)
-
-	e.GET("/path/:name", func(c echo.Context) error {
-		name := c.Param("name")
-		return c.String(200, name)
-	})
-
-	e.POST("/form", func(c echo.Context) error {
-		name := c.FormValue("name")
-		age, _ := strconv.Atoi(c.FormValue("age"))
-		return c.JSON(http.StatusOK, user{Name: name, Age: age})
-	})
-
-	//Bind()도 구조체 필드들이 대문자여야 가능
-	e.POST("/json", func(c echo.Context) error {
-		user := user{}
-		if err := c.Bind(&user); err != nil {
-			return c.String(http.StatusBadRequest, err.Error())
-		}
-		if user.Data == "" {
-			fmt.Println("Data 공백")
-			user.Data = "board"
-		}
-
-		fmt.Println(reflect.TypeOf(user.Background))
-
-		// user.Background = string(bd)
-
-		return c.JSON(http.StatusOK, user)
-	})
-
-	e.GET("/json/data", func(c echo.Context) error {
-		user := &user{
-			Name: "won",
-			Age:  27,
-			Data: "json 데이터 전송 테스트",
-		}
-
-		return c.JSON(http.StatusOK, user)
-	})
-
-	e.GET("/json/pretty", func(c echo.Context) error {
-		user := &user{
-			Name: "won",
-			Age:  27,
-			Data: "json 데이터 전송 테스트",
-		}
-
-		return c.JSONPretty(http.StatusOK, user, "	")
-	})
 
 	e.Logger.Fatal(e.Start(":3000"))
 }
